e88db13fa817858c1fcea00d3ba543af0f8cc832
VirusMauri1 <san231977@uvg.edu.gt>
2026-04-15 12:09:57 -0600

    se agregan getpostbyid y get userbyid para cumlir los gets RF02

--- js/api.js
+++ js/api.js
@@ -1,23 +1,44 @@
 // URL base de la API para los posts
 const BASE_URL = 'https://dummyjson.com/posts';
+const USERS_URL = 'https://dummyjson.com/users';
 
-// Get un post por su ID
+// GET todos los posts
 export const getPosts = async () => {
     try {
         const response = await fetch(BASE_URL);
-
-        if (!response.ok) {
-            throw new Error('Error al obtener posts');
-        }
-
+        if (!response.ok) throw new Error('Error al obtener posts');
         const data = await response.json();
         return data.posts;
-
     } catch (error) {
         console.error(error);
         throw error;
     }
 };
+
+// Get un post por su ID
+export const getPostById = async (id) => {
+    try {
+        const response = await fetch(`${BASE_URL}/${id}`);
+        if (!response.ok) throw new Error('Error al obtener el post');
+        return await response.json();
+    } catch (error) {
+        console.error('Error obteniendo el post:', error);
+        throw error;
+    }
+};
+
+// GET usuario por ID (RF-02)
+export const getUserById = async (id) => {
+    try {
+        const response = await fetch(`${USERS_URL}/${id}`);
+        if (!response.ok) throw new Error('Error al obtener el usuario');
+        return await response.json();
+    } catch (error) {
+        console.error('Error obteniendo el usuario:', error);
+        throw error;
+    }
+};
+
 
 // Delete un post por su ID
 export const deletePost = async (id) => {
